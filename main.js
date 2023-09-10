d8aefacf0610853bf8cf68f897d2ad8f5b4e3728
herintsaina <herintsainarak024@gmail.com>
2023-09-10 23:54:21 +0300

    Finalisation de ma portofolio apres lancement de ce site aux public

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ function checkedMenu() {
     location.reload();
 
 }
+
+// Sélectionnez le menu et les éléments du menu
+const menu = document.getElementById('menu-link'); // Remplacez 'menu' par l'ID de votre menu
+const menuItems = document.querySelectorAll('.menu-mobil'); // Remplacez '.menu-item' par la classe de vos éléments de menu
+
+// Ajoutez un gestionnaire d'événement de clic à chaque élément du menu
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Fermez le menu en masquant la classe CSS (par exemple, 'menu-active')
+    menu.classList.remove('menu-active'); // Assurez-vous de définir cette classe dans votre CSS
+  });
+});
